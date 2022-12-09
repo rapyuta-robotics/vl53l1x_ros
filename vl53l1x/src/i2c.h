40ee35583bae3e82c31f0277710d5ae025c78089
BhuvanChandra DV <bhuvanchandra.dv@rapyuta-robotics.com>
2022-12-09 11:57:45 +0900

    auto clang-formatter changes

--- vl53l1x/src/i2c.h
+++ vl53l1x/src/i2c.h
@@ -12,13 +12,13 @@ void i2c_setup(uint8_t bus, uint8_t addr);
 
 void i2c_release();
 
-EXTERNC bool i2c_readRegisterMulti(uint16_t reg, size_t count, void *pdata);
+EXTERNC bool i2c_readRegisterMulti(uint16_t reg, size_t count, void* pdata);
 
-EXTERNC bool i2c_readRegisterByte(int reg, uint8_t *pdata);
+EXTERNC bool i2c_readRegisterByte(int reg, uint8_t* pdata);
 
-EXTERNC bool i2c_readRegisterWord(uint16_t reg, uint16_t *pdata);
+EXTERNC bool i2c_readRegisterWord(uint16_t reg, uint16_t* pdata);
 
-EXTERNC bool i2c_writeRegisterMulti(uint16_t reg, size_t count, void *pdata);
+EXTERNC bool i2c_writeRegisterMulti(uint16_t reg, size_t count, void* pdata);
 
 EXTERNC bool i2c_writeRegisterByte(uint16_t reg, uint8_t value);
 
